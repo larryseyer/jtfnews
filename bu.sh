04f50adae6e78d2a8ca6356626d96fef67ee3de8
Larry Seyer <larryseyer@users.noreply.github.com>
2026-02-12 13:14:33 -0600

    Exclude .git from backup zip

--- bu.sh
+++ bu.sh
@@ -17,4 +17,4 @@ TIMESTAMP=$(date +"%Y_%m_%d_%H_%M_%S")
 MESSAGE=$(echo "$1" | head -1 | cut -c1-50 | sed 's/[^a-zA-Z0-9]/_/g' | sed 's/__*/_/g' | sed 's/_$//')
 ZIP_FILE="$DEST_DIR/JTFNews_${TIMESTAMP}_${MESSAGE}.zip"
 cd "$SOURCE" || exit 1
-zip -r "$ZIP_FILE" . -x "media/*" "media/" "gh-pages-dist/*" "gh-pages-dist/" "audio/*" "audio/" "data/*" "data/" "venv/*" "venv/" "__pycache__/*" "__pycache__/" ".env"
+zip -r "$ZIP_FILE" . -x "media/*" "media/" "gh-pages-dist/*" "gh-pages-dist/" "audio/*" "audio/" "data/*" "data/" "venv/*" "venv/" "__pycache__/*" "__pycache__/" ".git/*" ".git/" ".env"
